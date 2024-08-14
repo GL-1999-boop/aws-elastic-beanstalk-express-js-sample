8ae7ab84160676a2fd19008e7c5640db0f264d9a
GL-1999-boop <gowthamilakkoju@gmail.com>
2024-08-14 17:55:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World i like to do the code pipeline commit changes!'));
+app.get('/', (req, res) => res.send('Hello World to all aws partners'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
